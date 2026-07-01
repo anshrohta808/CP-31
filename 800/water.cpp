1101c7a96e6e75553924ac158e22d89f430dfb00
anshrohta808 <ansh.rohta808@gmail.com>
2026-07-01 09:50:00 +0530

    prob 1,2,3

---
+++ 800/water.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h> // include all standard headers
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+
+const int INF = 1e9;
+const ll INFLL = 1e18;
+const int MOD = 1e9 + 7;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // Declare any additional variables here
+
+    int t; // number of test cases
+    cin >> t;
+
+    while (t--)
+    {
+        // write your code here for each test case
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        bool checkThree = false;
+        int count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (i + 1 < n && i + 2 < n && s[i] == '.' && s[i + 1] == '.' && s[i + 2] == '.')
+            {
+                checkThree = true;
+                break;
+            }
+            if (s[i] == '.')
+                count++;
+        }
+        if (checkThree == true)
+            cout << 2 << endl;
+        else
+            cout << count << endl;
+    }
+
+    return 0;
+}
